0582dbdf39be427e9888146e7b45d56b531664da
San <tzurender@gmail.com>
2026-01-16 16:52:59 +0700

    updated main.py

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ def run_lyrics():
         ("Hindi na makalaya", 0.19),
         ("Dinadalaw mo 'ko bawat gabi", 0.12),
         ("Wala mang nakikita", 0.19),
-        ("Haplos mo'y ramdam pa rin sa dilim", 0.1),
+        ("Haplos mo'y ramdam pa rin sa dilim", 0.08),
         ("Hindi na nananaginip", 0.19),
         ("Hindi na ma-makagising", 0.16),
         ("Pasindi na ng ilaw", 0.2),
         ("Minumulto na 'ko ng damdamin ko", 0.1),
         ("Ng damdamin ko", 0.12),
         ("Hindi mo ba ako lilisanin?", 0.15),
-        ("Hindi pa ba sapat pagpapahirap sa 'kin? (Damdamin ko)", 0.08),
+        ("Hindi pa ba sapat pagpapahirap sa 'kin? (Damdamin ko)", 0.05),
         ("Hindi na ba ma-mamamayapa?", 0.14),
         ("Hindi na ba ma-mamamayapa?", 0.14)
     ]
     
-    line_delays = [1, 1.5, 1, 1.3, 1, 1, 1, 0.6, 0.1, 0.9, 0.6, 0.6, 0.6]
+    line_delays = [1, 1.5, 1, 1.3, 0.8, 1, 1, 0.4, 0.1, 0.9, 0.6, 0.6, 0.6]
 
     print("\nMulto - Cup of Joe \n")
     time.sleep(2)
